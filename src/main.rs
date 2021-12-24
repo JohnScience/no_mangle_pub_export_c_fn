5a336114841cc472564ef6aff4096d95c6122248
Dmitrii - Demenev <demenev.dmitriy1@mail.ru>
2021-12-24 13:15:02 -0700

    Finished polishing the lib

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use no_mangle_pub_export_c_fn::{parse_for_no_mangle_pub_extern_c_fns, ParsedFile};
 
-mod lib;
-
 fn main() {
     let crate_root = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     let parsed_files: Vec<ParsedFile> = parse_for_no_mangle_pub_extern_c_fns(crate_root.as_str());
